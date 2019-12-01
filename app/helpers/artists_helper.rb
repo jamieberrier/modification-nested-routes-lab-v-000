3f960ad68120bdc8169164eeaa3d35bae0197b24
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-01 21:18:57 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,8 +2,8 @@ module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
-  # a helper to display a drop-down list of artists if someone edits
-  # a song directly via /songs/id/edit and to only display the artist's name if they are editing through nested routing
+  # a helper to display a drop-down list of artists if someone edits a song directly via /songs/id/edit
+  # and to only display the artist's name if they are editing through nested routing
   def artist_select(song, path)
     if path == "nested"
       hidden_field_tag "song[artist_id]", song.artist_id

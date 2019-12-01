b68b29bccd6e7f812c4f00009e20a3eba5de106b
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-01 20:21:39 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,9 +1,9 @@
 module SongsHelper
-  def author_id_field(post)
-    if post.author.nil?
-      select_tag "post[author_id]", options_from_collection_for_select(Author.all, :id, :name)
+  def artist_id_field(song)
+    if song.artist.nil?
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     else
-      hidden_field_tag "post[author_id]", post.author_id
+      hidden_field_tag "song[artist_id]", song.artist_id
     end
   end
 end

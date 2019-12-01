de22c211b5d8fecf8e77e0038fdd152daaf60d46
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-01 20:39:21 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -4,10 +4,10 @@ module ArtistsHelper
   end
 
   def artist_select(song, path)
-    if song.artist.nil?
-      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
-    else
+    if song.artist && path == "nested"
       hidden_field_tag "song[artist_id]", song.artist_id
+    else
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     end
   end
 end

edfd6ef943a6058946b2fccb1b7256f266a51c20
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-01 22:11:22 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :artists do
     resources :songs, only: [:index, :show, :new, :edit]
   end
+  # Set up routes and controller actions to support editing a song as a nested resource of an artist.
   resources :songs do
     resources :artists, only: [:edit]
   end
